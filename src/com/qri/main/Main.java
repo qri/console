e4338134c3f180fcae8e7d3559d29a599af6964d
Qaiser Riaz <qri@startext.de>
2013-06-25 12:27:23 +0200

    added source code

---
+++ src/com/qri/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.qri.main;
+
+/**
+ * @author qri
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("A test console app for GitHub");
+	}
+
+}
